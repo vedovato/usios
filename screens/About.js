68c17ea1123509963a5100290a970df0454ebfb2
Andrei Vedovato <vedovato.andrei@gmail.com>
2019-11-16 17:04:24 -0300

    add link to List

--- screens/About.js
+++ screens/About.js
@@ -4,9 +4,7 @@ import {styles} from './Home'
 
 import { pushScreen } from '../utils/navigation'
 
-export default props => {
-  console.warn('RECEBI', props)
-
+const About = props => {
   return (
     <View>
       <Text>Tela #2</Text>
@@ -15,21 +13,19 @@ export default props => {
         style={styles.btnWrapper}
         onPress={() => pushScreen({
           id: props.componentId,
-          name: 'biobox.contact',
-          passProps: {
-            pokemon: 'vai pikachu!',
-            teste: () => console.warn('FOI MERMO!')
-          },
-          options: {
-            topBar: {
-              visible: false,
-              title: { text: 'AE CARAI' },
-              subtitle: { text: 'Sucesso!' }
-            }
-          }
+          name: 'biobox.list',
+          // options: {
+            // topBar: {
+              // visible: false,
+              // title: { text: 'AE CARAI' },
+              // subtitle: { text: 'Sucesso!' }
+            // }
+          // }
         })}>
-          <Text style={styles.btnText}>Ir para Tela #3</Text>
+          <Text style={styles.btnText}>Acessar Lista</Text>
         </TouchableHighlight>
       </View>
   )
 }
+
+export default About
