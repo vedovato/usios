fa473dd81d91ce3927a9707b29f73a983655898c
Andrei Vedovato <vedovato.andrei@gmail.com>
2019-11-16 17:04:46 -0300

    add id param to API URL

--- utils/api.js
+++ utils/api.js
@@ -1,14 +1,15 @@
-const API = async ({acao, operacao, dados}) => {
+const API = async ({acao, operacao, dados, id}) => {
   const SENHA = 'senha=Usios*BioBox@2019';
   const BASE = 'http://www.biobox.net.br/sistema/api';
   let URI = `${BASE}/gerenciador.php?${SENHA}&acao=${acao}`;
 
   operacao ? (URI = `${URI}&operacao=${operacao}`) : null;
   dados ? (URI = `${URI}&dados=${dados}`) : null;
+  id ? (URI = `${URI}&id=${id}`) : null;
 
-  // console.warn('URL', URI)
+  // console.warn('URL', URI);
   const raw = await fetch(URI);
   return raw.json();
 };
 
-export default API
+export default API;
